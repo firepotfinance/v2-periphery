c6b317d3a09a236b832a14ff7f8e701de32b0886
sebn <sebn@firepot.finance>
2022-09-14 15:17:25 -0500

    changed init hash code, removed remix build info

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'a02108a20103c6b7994b253834b7e2d8ee2eae0400220a2074fe8d885ce59fce' // init code hash
             ))));
     }
 
